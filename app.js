c44f2d4c91cb474d24f4c69efc5c51519baa74cb
OleksandrVitvitsky <vitvitsky.oleksandr@gmail.com>
2023-06-01 22:56:59 +0300

    add 1

--- app.js
+++ app.js
@@ -12,25 +12,25 @@
 
 
 
-const path_without_promice = require('node:path');
-const fs_without_promice = require('node:fs');
+const path = require('node:path');
+const fs = require('node:fs');
 
 for (let i = 0; i < 5; i++) {
     const NewFolder = `folder${i+1}`;
 
-    fs_without_promice.mkdir(path_without_promice.join(__dirname, 'HomeWork1', NewFolder), (err) => {
+    fs.mkdir(path.join(__dirname, 'HomeWork1', NewFolder), (err) => {
             if (err) throw  new Error(err.message);
         })
 
-    fs_without_promice.writeFile(path_without_promice.join(__dirname, 'HomeWork1', NewFolder, `file_in_${NewFolder}.txt`), `this file in ${NewFolder}`, (err) => {
+    fs.writeFile(path.join(__dirname, 'HomeWork1', NewFolder, `file_in_${NewFolder}.txt`), `this file in ${NewFolder}`, (err) => {
             if (err) throw  new Error(err.message);
         })
 
 }
 
 
-fs_without_promice.readdir(
-    path_without_promice.join(__dirname, 'HomeWork1'), {withFileTypes: true}, (err, files) => {
+fs.readdir(
+    path.join(__dirname, 'HomeWork1'), {withFileTypes: true}, (err, files) => {
         if (err) throw  new Error(err.message);
         files.forEach(file => {
                 let typeFile = 'undefined';
@@ -40,8 +40,8 @@ fs_without_promice.readdir(
                     typeFile = 'File';
                 console.log(`${typeFile}: {${file.name}}`);
 
-                fs_without_promice.readdir(
-                    path_without_promice.join(__dirname, 'HomeWork1', file.name), {withFileTypes: true}, (err, files) => {
+                fs.readdir(
+                    path.join(__dirname, 'HomeWork1', file.name), {withFileTypes: true}, (err, files) => {
                         if (err) throw  new Error(err.message);
                         files.forEach(file => {
                                 let typeFile = 'undefined';
@@ -58,80 +58,3 @@ fs_without_promice.readdir(
         )
     })
 
-//***********************************************************************************************************************
-// resolve by Max
-
-const fs = require('node:fs/promises');
-const path = require('node:path');
-//
-// const foo = async () => {
-//     const basePath = path.join(process.cwd(), 'baseFolder');
-//
-//     await fs.mkdir(basePath, {recursive: true});
-//     const fileNames = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt'];
-//     const folderNames = ['folder1', 'folder2', 'folder3', 'folder4'];
-//
-//     for (const file of fileNames) {
-//         await fs.writeFile(path.join(basePath, file), 'HELLO');
-//     }
-//     for (const folder of folderNames) {
-//         await fs.mkdir(path.join(basePath, folder), {recursive: true});
-//     }
-//
-//     const files = await fs.readdir(basePath);
-//     for (const file of files) {
-//         const stat = await fs.stat(path.join(basePath, file));
-//         console.log(path.join(basePath, file), ' : ', stat.isDirectory() ? 'folder' : 'file');
-//     }
-// }
-// foo();
-
-
-// const foo = async () => {
-//     const basePath = path.join(process.cwd(), 'baseFolder');
-//
-//     await fs.mkdir(basePath, {recursive: true});
-//     const fileNames = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt'];
-//     const folderNames = ['folder1', 'folder2', 'folder3', 'folder4'];
-//
-//     for (const folder of folderNames) {
-//         const folderPath = path.join(basePath, folder);
-//         await fs.mkdir(folderPath, {recursive: true});
-//
-//         for (const file of fileNames) {
-//             await fs.writeFile(path.join(folderPath, file), 'HELLO');
-//         }
-//     }
-//
-//     const files = await fs.readdir(basePath);
-//     for (const file of files) {
-//         const stat = await fs.stat(path.join(basePath, file));
-//         console.log(path.join(basePath, file), ' : ', stat.isDirectory() ? 'folder' : 'file');
-//     }
-// }
-// foo();
-
-
-const foo = async () => {
-    const basePath = path.join(process.cwd(), 'baseFolder');
-
-    await fs.mkdir(basePath, {recursive: true});
-    const fileNames = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt', 'file6.txt'];
-    const folderNames = ['folder1', 'folder2', 'folder3', 'folder4'];
-
-    await Promise.all(folderNames.map(async (folder) => {
-        const folderPath = path.join(basePath, folder);
-        await fs.mkdir(folderPath, {recursive: true});
-
-        await Promise.allSettled(fileNames.map(async (file) => {
-            await fs.writeFile(path.join(folderPath, file), 'HELLO');
-        }));
-    }));
-
-    const files = await fs.readdir(basePath);
-    for (const file of files) {
-        const stat = await fs.stat(path.join(basePath, file));
-        console.log(path.join(basePath, file), ' : ', stat.isDirectory() ? 'folder' : 'file');
-    }
-}
-foo();
